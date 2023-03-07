0c1316acc412e5f81f4282dcc5794b89dd396f19
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-03-07 20:37:53 -0300

    feat: init basics

--- main.go
+++ main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+type Order struct {
+	ID       string
+	Price    float64
+	Quantity int
+}
+
+func (order *Order) getTotal() float64 {
+	return order.Price * float64(order.Quantity)
+}
+
+func (order Order) setPrice(price float64) {
+	order.Price = price
+
+	fmt.Println("Price: ", order.Price)
+}
+
 func main() {
-	fmt.Println("Hello")
+	order := Order{
+		ID:       "123",
+		Price:    10.5,
+		Quantity: 5,
+	}
+
+	order.setPrice(74.89)
+	fmt.Println(order.getTotal())
 }
