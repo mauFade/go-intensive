6731184b7319875202df44518ee7198e8adbd43c
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-03-08 20:46:57 -0300

    feat: add unit tests to order entity

---
+++ internal/entity/order_test.go
@@ -0,0 +1,54 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIfIdIsBlank(t *testing.T) {
+	order := &Order{}
+
+	assert.Error(t, order.Validate(), "Order id is required")
+}
+
+func TestIfPriceIsBlank(t *testing.T) {
+	order := &Order{}
+
+	order.ID = "ID"
+
+	assert.Error(t, order.Validate(), "Order price is required")
+}
+
+func TestIfTaxIsBlank(t *testing.T) {
+	order := &Order{}
+
+	order.ID = "ID"
+	order.Price = 15.55
+
+	assert.Error(t, order.Validate(), "Order tax is required")
+}
+
+func TestValidParams(t *testing.T) {
+	order := &Order{
+		ID:    "123",
+		Price: 10.0,
+		Tax:   5.0,
+	}
+
+	assert.NoError(t, order.Validate())
+
+	assert.Equal(t, "123", order.ID)
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 5.0, order.Tax)
+}
+
+func TestCalculateFinalPrice(t *testing.T) {
+	order := &Order{
+		ID:    "123",
+		Price: 0.0,
+		Tax:   0.0,
+	}
+
+	assert.Error(t, order.CalculateFinalPrice(), "Order price is required")
+}
