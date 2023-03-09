8b56f7d5b2c32bbc8ec2dd982bd6c50f4966ba98
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-03-08 21:28:21 -0300

    feat: init database integration

--- internal/infra/database/order-repository.go
+++ internal/infra/database/order-repository.go
@@ -1,6 +1,10 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/mauFade/go-intensive/internal/entity"
+)
 
 type OrderRepository struct {
 	DB *sql.DB
@@ -10,4 +14,26 @@ func NewOrderRepository(db *sql.DB) *OrderRepository {
 	return &OrderRepository{
 		DB: db,
 	}
+}
+
+func (repository *OrderRepository) Save(order *entity.Order) error {
+	_, err := repository.DB.Exec("INSERT INTO orders (id, price, tax, final_price), VALUES(?,?,?,?)", order.ID, order.Price, order.Tax, order.FinalPrice)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (repository *OrderRepository) GetTotalOrdersQuantity() (int, error) {
+	var total int
+
+	err := repository.DB.QueryRow("SELECT COUNT(*) FROM orders").Scan(&total)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return total, nil
 }
