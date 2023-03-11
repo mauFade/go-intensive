9e48a894580e463db45e5e35c5a067bceab4df3a
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-03-11 20:23:21 -0300

    feat: start kafka consumer

---
+++ pkg/kafkaclient/kafka.go
@@ -0,0 +1,25 @@
+package kafkaclient
+
+import ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+
+func Consume(topics []string, server string, msgChannel chan *ckafka.Message) {
+	consumer, err := ckafka.NewConsumer(ckafka.ConfigMap{
+		"bootstrap.servers": server,
+		"group.id":          "goapp",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = consumer.SubscribeTopics(topics, nil)
+
+	for {
+		msg, err := consumer.ReadMessage(-1)
+
+		if err == nil {
+			msgChannel <- msg
+		}
+	}
+}
